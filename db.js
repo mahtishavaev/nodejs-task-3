e6ed0db05b55f3077d73dc828433ea0e019ac3b7
Mahti <mahtishavaev@gmail.com>
2021-05-18 08:26:35 +0300

    fix: export from db.js

--- db.js
+++ db.js
@@ -1,17 +1,24 @@
-const Sequelize = require("sequelize");
-require("dotenv").config();
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+);
 
 sequelize.authenticate().then(
   function success() {
-    console.log("Connected to DB");
+    console.log('Connected to DB');
   },
 
   function fail(err) {
     console.log(`Error: ${err}`);
   }
 );
+
+module.exports = sequelize;
